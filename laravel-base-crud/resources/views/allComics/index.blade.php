1f7b5f618d681b9d3c507713e826d3a8c79c8129
Stefano Calzoni <stefanoclz@hotmail.it>
2022-05-25 16:04:47 +0200

    stampati a schermo fumetti tramite resource controller

---
+++ laravel-base-crud/resources/views/allComics/index.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/app.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <h1>All Comics</h1>
+    <div class="container">
+        @foreach ($comicsAll as $comic)
+            <div class="card">
+                <img src="{{ $comic->thumb }}" alt="Comic">
+                <h2>{{ $comic->title }}</h2>
+                <h3>{{ $comic->series }}</h3>
+                <h3>{{ $comic->price }}</h3>
+                <h5>{{ $comic->sale_date }}</h5>
+                <div class="text">
+                    <p>{{ $comic->description }}</p>
+                </div>
+            </div>
+        @endforeach
+    </div>
+</body>
+
+</html>
